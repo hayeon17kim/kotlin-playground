d1e9e5f04aa8f9c9ef66d5ff3e43ab9f60f6289b
Hayeon Kim <hayeon17kim@gmail.com>
2021-10-03 01:45:46 +0900

    add : validation to parameters

--- kotlin-spring/src/main/kotlin/com/monica/kotlinspring/controller/delete/DeleteApiController.kt
+++ kotlin-spring/src/main/kotlin/com/monica/kotlinspring/controller/delete/DeleteApiController.kt
@@ -1,6 +1,9 @@
 package com.monica.kotlinspring.controller.delete
 
 import org.springframework.web.bind.annotation.*
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 @RestController
 @RequestMapping("/api")
@@ -12,6 +15,9 @@ class DeleteApiController {
     @DeleteMapping(path = ["/delete-mapping"])
     fun deletemapping(
         @RequestParam(value = "name") _name : String,
+
+        @NotNull(message  = "age 값이 누락되었습니다.")
+        @Min(value = 20, message = "age는 20보다 커야 합니다.")
         @RequestParam(value = "age") _age : Int
     ): String{
         println(_name)
@@ -20,7 +26,14 @@ class DeleteApiController {
     }
 
     @DeleteMapping(path = ["/delete-mapping/name/{name}/age/{age}"])
-    fun deleteMapping(@PathVariable(value = "name") _name:String, @PathVariable(name = "age") _age:Int): String {
+    fun deleteMapping(@PathVariable(value = "name")
+                      @Size(min = 2, max = 5, message = "name의 길이는 2-5")
+                      @NotNull
+                      _name:String,
+
+                      @NotNull(message  = "age 값이 누락되었습니다.")
+                      @Min(value = 20, message = "age는 20보다 커야 합니다.")
+                      @PathVariable(name = "age") _age:Int): String {
         println(_name)
         println(_age)
         return "$_name $_age"
