bed550c077f1bd2d9d21517dc5a71963b4f002f5
Hayeon Kim <monica@daangn.com>
2021-10-07 23:43:53 +0900

    add : test

--- kotlin-spring/src/test/kotlin/com/monica/kotlinspring/KotlinSpringApplicationTests.kt
+++ kotlin-spring/src/test/kotlin/com/monica/kotlinspring/KotlinSpringApplicationTests.kt
@@ -1,13 +1,31 @@
 package com.monica.kotlinspring
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
 
+@WebMvcTest
 @SpringBootTest
 class KotlinSpringApplicationTests {
 
+	@Autowired
+	lateinit var mockMvc : MockMvc
+
 	@Test
-	fun contextLoads() {
+	fun helloTest() {
+			mockMvc.perform(
+				get("/api/exception/hello")
+			).andExpect {
+				status().isOk
+			}.andExpect(
+				content().string("hello")
+			).andDo(print())
+		}
 	}
 
 }
